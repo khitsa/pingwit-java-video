8ca850a94dc000e3f706148c8ef61732dc0fbae0
Alexander Fomin <RezviyBelorus@gmail.com>
2024-03-09 17:15:25 +0100

    Update Homework.java

--- src/com/pingwit/part_1/homework/task_4/Homework.java
+++ src/com/pingwit/part_1/homework/task_4/Homework.java
@@ -1,2 +1,2 @@
-package com.pingwit.part_1.homework.task_4;public class Homework {
+package com.pingwit.part_1.homework.task_4;public class Homework { // public class Homework - это с новой строки, а где решение ?)
 }
