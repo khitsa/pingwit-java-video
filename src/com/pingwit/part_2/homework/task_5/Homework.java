56b3fed36be62e0a7ec163d0b255f93091a4eb9a
Alexander Fomin <RezviyBelorus@gmail.com>
2024-03-09 18:57:58 +0100

    Update Homework.java

--- src/com/pingwit/part_2/homework/task_5/Homework.java
+++ src/com/pingwit/part_2/homework/task_5/Homework.java
@@ -2,7 +2,7 @@ package com.pingwit.part_2.homework.task_5;
 
 public class Homework {
     public static void main(String[] args) {
-        int expression = 256 / 8;
+        int expression = 256 / 8; // для остатка от деления нужен оператор %
         System.out.println(expression);
     }
 }
