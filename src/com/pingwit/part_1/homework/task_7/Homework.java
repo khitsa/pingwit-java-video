e9c630f9bd866394c06c31b0c5747c25086ef0b2
Alexander Fomin <RezviyBelorus@gmail.com>
2024-03-09 18:56:50 +0100

    Update Homework.java

--- src/com/pingwit/part_1/homework/task_7/Homework.java
+++ src/com/pingwit/part_1/homework/task_7/Homework.java
@@ -2,7 +2,7 @@ package com.pingwit.part_1.homework.task_7;
 
 public class Homework {
     public static void main(String[] args) {
-    int expression = 56 / 8;
+    int expression = 56 / 8; // остаток от деления дает оператор %, у тебя обычное деление двух целых чисел. 56/8 = 7 и программа у тебя распечает 7, а остаток от деления у тебя = 0
     System.out.println(expression);
- }
+ }//возможно код не отформатирован
 }
