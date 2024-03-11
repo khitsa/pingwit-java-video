7c7a6ecc4db864b27b5551fd8d87fa46040d628b
dimakhitsa <dimavideo2020@icloud.com>
2024-03-11 15:23:11 +0100

    part_1 - part_5

--- src/com/pingwit/part_5/DebugExample.java
+++ src/com/pingwit/part_5/DebugExample.java
@@ -1,4 +1,15 @@
 package com.pingwit.part_5;
 
 public class DebugExample {
+    public static void main(String[] args) {
+        int sum = 0;
+
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 ==0) {
+                sum += i;
+            }
+        }
+
+        System.out.println(sum);
+    }
 }
