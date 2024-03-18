8d591f96196669b9990186ef0d541e9037db2883
dimakhitsa <dimavideo2020@icloud.com>
2024-03-18 22:34:28 +0100

    par_4 homework task_1 ref cod

--- src/com/pingwit/part_5/homework/task_3/homework/Butterfly.java
+++ src/com/pingwit/part_5/homework/task_3/homework/Butterfly.java
@@ -5,43 +5,43 @@ import java.util.Scanner;
 public class Butterfly {
     public static void main(String[] args) {
 
-                Scanner scanner = new Scanner(System.in);
-                System.out.print("Введите размер бабочки: ");
-                int size = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите размер бабочки: ");
+        int size = scanner.nextInt();
 
-                drawButterfly(size);
+        drawButterfly(size);
+    }
+
+    public static void drawButterfly(int size) {
+        int halfSize = size / 2;
+
+        for (int i = 1; i <= halfSize; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
             }
-
-            public static void drawButterfly(int size) {
-                int halfSize = size / 2;
-
-                for (int i = 1; i <= halfSize; i++) {
-                    for (int j = 1; j <= i; j++) {
-                        System.out.print(j);
-                    }
-                    for (int j = 1; j <= 2 * (halfSize - i) + 1; j++) {
-                        System.out.print(" ");
-                    }
-                    for (int j = i; j >= 1; j--) {
-                        System.out.print(j);
-                    }
-                    System.out.println();
-                }
-
-                for (int i = halfSize; i >= 1; i--) {
-                    for (int j = 1; j <= i; j++) {
-                        System.out.print(j);
-                    }
-                    for (int j = 1; j <= 2 * (halfSize - i) + 1; j++) {
-                        System.out.print(" ");
-                    }
-                    for (int j = i; j >= 1; j--) {
-                        System.out.print(j);
-                    }
-                    System.out.println();
-                }
-           }
+            for (int j = 1; j <= 2 * (halfSize - i) + 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
         }
+
+        for (int i = halfSize; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (int j = 1; j <= 2 * (halfSize - i) + 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
 
 
 
