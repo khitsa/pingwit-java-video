19019ec9454e348d791a9e5ba555121f2bae1c56
dimakhitsa <dimavideo2020@icloud.com>
2024-03-19 20:24:13 +0100

    part_4 homework task_2

--- src/com/pingwit/part_4/homework/task_2/Homework.java
+++ src/com/pingwit/part_4/homework/task_2/Homework.java
@@ -1,4 +1,15 @@
 package com.pingwit.part_4.homework.task_2;
 
 public class Homework {
+    public static void main(String[] args) {
+        int hours = 24;
+        int amoeba = 1;
+        int time = 3;
+
+        while (time <= hours) {
+            amoeba *= 2;
+            System.out.println("Через " + time + " часов будет " + amoeba + "амеб(ы).");
+            time += 3;
+        }
+    }
 }
