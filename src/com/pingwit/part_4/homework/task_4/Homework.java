5b289a2f1188c05694c92e6b539c52bd5ca9733c
dimakhitsa <dimavideo2020@icloud.com>
2024-03-21 00:30:29 +0100

    part_4 homework task_4

---
+++ src/com/pingwit/part_4/homework/task_4/Homework.java
@@ -0,0 +1,26 @@
+package com.pingwit.part_4.homework.task_4;
+
+public class Homework {
+        public static void main(String[] args) {
+            int a = 5;
+            int b = 4;
+            int result = multiply(a, b);
+            System.out.println(a + " * " + b + " = " + result);
+        }
+
+        public static int multiply(int a, int b) {
+            int result = 0;
+            boolean isNegative = (a < 0 && b > 0) || (a > 0 && b < 0);
+
+            a = Math.abs(a);
+            b = Math.abs(b);
+
+            while (b > 0) {
+                result += a;
+                b--;
+            }
+
+            return isNegative ? -result : result;
+        }
+    }
+
