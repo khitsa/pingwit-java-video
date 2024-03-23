63cd8ec7ca52acb58a75d28e47263a526dfbc555
dimakhitsa <dimavideo2020@icloud.com>
2024-03-23 21:38:42 +0100

    part_7 ref cod

--- src/com/pingwit/part_7/Student.java
+++ src/com/pingwit/part_7/Student.java
@@ -10,10 +10,30 @@ public class Student {
     Boolean expelled;
     boolean happy;
 
+    static {
+        System.out.println("Static init block 1");
+        message = "dfdf";
+    }
+
+    static {
+        System.out.println("Static init block 2");
+    }
+
+
+    {
+        System.out.println("Init block 1");
+        happy = true;
+    }
+
+    {
+        System.out.println("Init block 2");
+    }
+
     public Student(String name, String surname) {
         System.out.println("Hello from constructor");
         this.name = name;
         this.surname = surname;
+        this.happy = false;
     }
 
     public Student(String name, int age) {
@@ -24,5 +44,19 @@ public class Student {
     public Student() {
         this("RandomName", "RandomSurname");
         System.out.println("Hello from default constructor");
+    }
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", group='" + group + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                ", expelled=" + expelled +
+                ", happy=" + happy +
+                '}';
     }
 }
